fae6e61c66c34b4b953255e43505a45c43d16608
HenL188 <153272326+HenL188@users.noreply.github.com>
2025-12-28 20:53:38 -0700

    first commit

---
+++ packages.sh
@@ -0,0 +1,9 @@
+emerge --ask --deepclean dev-vcs/git
+emerge --deepclean app-portage/cpuid2cpuflags
+emerge --ask dev-vcs/git
+emerge --ask sys-apps/systemd-utils
+emerge --ask sys-auth/elogind
+emerge --ask dev-lang/go
+emerge --ask dev-lang/zig
+emerge --ask dev-lang/zig-bin
+emerge --ask app-misc/neofetch
